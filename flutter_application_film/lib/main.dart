adb2ef835c21bd10661630bc6537accec3c422e3
VincentiusFernando <vincentiusfernando_2226240032@mhs.mdp.ac.id>
2025-03-17 16:02:22 +0700

    test

--- flutter_application_film/lib/main.dart
+++ flutter_application_film/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_film/screens/favorite_screen.dart';
 import 'package:flutter_application_film/screens/home_screen.dart';
+import 'package:flutter_application_film/screens/search_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,6 +19,40 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       home: const HomeScreen(),
+    );
+  }
+}
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  int _selectedIndex = 0;
+
+  final List<Widget> _screens = [
+    const HomeScreen(),
+    const SearchScreen(),
+    const FavoriteScreen()
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _screens[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _selectedIndex,
+          onTap: _onItemTapped,
+          items: [BottomNavigationBarItem(icon: Icon(Icons.home))]),
     );
   }
 }
